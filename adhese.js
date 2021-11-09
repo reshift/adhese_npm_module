cc3668b445bf554ee0079eb9b8a14b1ad95e1ec6
Tim Sturtewagen <tim@adhese.eu>
2021-11-09 09:56:22 +0100

    Update adhese.js

--- adhese.js
+++ adhese.js
@@ -200,11 +200,4 @@ function btoa(str) {
   return buffer.toString('base64');
 }
 
-function uuid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-
 exports.getAdheseAds = getAdheseAds;
